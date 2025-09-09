8e6530dd4510f8eb5d73d4ffcff989883f008a17
Ardee Aram <ardee.aram@gmail.com>
2025-09-09 23:35:25 +0800

    Basic React UI

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,5 +1,16 @@
 import React from "react"
 
 export default function App() {
-  return <h1>If you are reading this, it means React is rendering 🎉</h1>
+  return (
+    <div className="mx-auto w-3/4">
+      <h1 className="block text-2xl">Cash Register</h1>
+      <div className="h-50 border-blue-400 border-1 rounded-md w-full"></div>
+      <div className="h-20 rounded-md w-full text-3xl"> = €0</div>
+      <div>
+        <button className="bg-blue-400 text-white cursor-pointer p-3 m-2 rounded-full">GR1 - Green Tea</button>
+        <button className="bg-blue-400 text-white cursor-pointer p-3 m-2 rounded-full">SR1 - Strawberries</button>
+        <button className="bg-blue-400 text-white cursor-pointer p-3 m-2 rounded-full">CF1 - Coffee</button>
+      </div>
+    </div>   
+  )
 }
